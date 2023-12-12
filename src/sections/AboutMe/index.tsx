2bb3369196d32e1fea5753d205ed239b67ed1e1f
Davidson Marra <davidson.vieira@stone.com.br>
2023-12-12 17:51:19 -0300

    chore: att infos

--- src/sections/AboutMe/index.tsx
+++ src/sections/AboutMe/index.tsx
@@ -1,22 +1,23 @@
-import React, { useEffect, useRef } from 'react';
-import { ScrollReveal } from '../../components/ScrollReveal';
-import { SectionText } from '../../components/SectionText';
-import Typed from 'typed.js';
-import { ResumeButton } from '../../components/ResumeButton';
-import { Container, 
-  Content, 
-  Image, 
-  TextContainer, 
+import React, { useEffect, useRef } from "react";
+import { ScrollReveal } from "../../components/ScrollReveal";
+import { SectionText } from "../../components/SectionText";
+import Typed from "typed.js";
+import { ResumeButton } from "../../components/ResumeButton";
+import {
+  Container,
+  Content,
+  Image,
+  TextContainer,
   TitleContainer,
-  Title, 
+  Title,
   TitleDynamic,
   DescriptionContainer,
   Divider,
   Description,
-  Qualifications
-} from './styles';
-import { TimeLine } from '../../components/TimeLine';
-import { Anchor } from '../../components/Anchor';
+  Qualifications,
+} from "./styles";
+import { TimeLine } from "../../components/TimeLine";
+import { Anchor } from "../../components/Anchor";
 
 interface AboutMeProps {
   themeMode: boolean;
@@ -28,13 +29,14 @@ export const AboutMe: React.FC<AboutMeProps> = ({ themeMode }) => {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["Desenvolvedor Front-End", 
-        "Desenvolvedor Mobile", 
-        "Desenvolvedor Back-End", 
+      strings: [
+        "Desenvolvedor Front-End",
+        "Desenvolvedor Mobile",
+        "Desenvolvedor Back-End",
         "Desenvolvedor Full-Stack",
         "Freelancer",
-        "Técnico em eletrônica"
-      ], 
+        "Técnico em eletrônica",
+      ],
       startDelay: 300,
       typeSpeed: 100,
       backSpeed: 100,
@@ -52,24 +54,11 @@ export const AboutMe: React.FC<AboutMeProps> = ({ themeMode }) => {
   return (
     <Container>
       <Anchor id="about" />
-      <ScrollReveal 
-        origin="top"
-        distance="3rem"
-      >
-        <SectionText 
-          title="Sobre mim"
-          subTitle="quem eu sou"
-        />
-        <ScrollReveal
-        origin="top"
-        distance="3rem"
-        delay={1000}
-        >
-        <Content>
-            <Image
-              src={uri}
-              alt="Foto de Davidson Marra"
-            />
+      <ScrollReveal origin="top" distance="3rem">
+        <SectionText title="Sobre mim" subTitle="quem eu sou" />
+        <ScrollReveal origin="top" distance="3rem" delay={1000}>
+          <Content>
+            <Image src={uri} alt="Foto de Davidson Marra" />
             <TextContainer>
               <TitleContainer>
                 <Title>Eu sou &nbsp;</Title>
@@ -78,34 +67,27 @@ export const AboutMe: React.FC<AboutMeProps> = ({ themeMode }) => {
               <DescriptionContainer>
                 <Divider />
                 <Description>
-                  Atualmente sou Programador em desenvolvimento Mobile com React Native na Compasso UOL e estou em busca de adquirir experiências e aumentar meus conhecimentos na área. <br/>
-                  Estou cursando Engenharia da Computação na PUC Minas e fiz curso técnico integrado de eletrônica no COLTEC.<br/>
-                  Meu foco atual é o React Native no ambiente mobile, no qual eu trabalho e o ReactJS na web.
+                  Sou um apaixonado Software Engineer na Stone, especializado em
+                  desenvolvimento mobile. Minha jornada profissional é centrada
+                  na criação de experiências excepcionais para usuários,
+                  combinando habilidades técnicas sólidas com uma abordagem
+                  criativa e inovadora. Atuo principalmente com tecnologias como
+                  React Native, Swift, Kotlin e Kotlin Multiplatform.
                 </Description>
               </DescriptionContainer>
               <ResumeButton themeMode={themeMode} />
             </TextContainer>
-        </Content>
+          </Content>
+        </ScrollReveal>
       </ScrollReveal>
-      </ScrollReveal>
-      <ScrollReveal 
-        origin="top"
-        distance="3rem"
-      >
+      <ScrollReveal origin="top" distance="3rem">
         <Qualifications>
-          <SectionText 
-            title="Qualificação"
-            subTitle="minha jornada"
-          />
-          <ScrollReveal
-            origin="top"
-            distance="3rem"
-            delay={1000}
-          >
+          <SectionText title="Qualificação" subTitle="minha jornada" />
+          <ScrollReveal origin="top" distance="3rem" delay={1000}>
             <TimeLine />
           </ScrollReveal>
-          </Qualifications>
+        </Qualifications>
       </ScrollReveal>
     </Container>
   );
-}
+};
--- src/utils/timeLineData.ts
+++ src/utils/timeLineData.ts
@@ -3,45 +3,45 @@ export const dataAcademy = [
     title: "Engenharia da Computação",
     local: "PUC Minas",
     date: "2021 - 2025",
-    id: 1
+    id: 1,
   },
   {
     title: "Técnico de Eletrônica",
     local: "Coltec - UFMG",
     date: "2017 - 2019",
-    id: 2
-  }
-]
+    id: 2,
+  },
+];
 
 export const dataProfessional = [
   {
-    title: "Dev Mobile",
-    local: "Ioasys",
-    date: "2022 - o momento",
-    id: 1
+    title: "Software Engineer III",
+    local: "Stone Co.",
+    date: "2023 - o momento",
+    id: 1,
   },
   {
-    title: "Dev Mobile - Programador 1",
+    title: "Dev Mobile",
+    local: "Ioasys",
+    date: "2022 - 2023",
+    id: 2,
+  },
+  {
+    title: "Dev FullStack",
     local: "Compasso UOL",
     date: "2021 - 2022",
-    id: 2
+    id: 3,
   },
   {
     title: "Dev Mobile - Trainee",
     local: "Compasso UOL",
     date: "2021",
-    id: 3
-  },
-  {
-    title: "Dev Front-End - Estagiário",
-    local: "Compasso UOL",
-    date: "2021",
-    id: 4
+    id: 4,
   },
   {
     title: "Técnico de Eletrônica",
     local: "SolarView",
     date: "2019 - 2020",
-    id: 5
-  }
-]
+    id: 5,
+  },
+];
